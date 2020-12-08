511f0542b0b3f146d0a7abedef68d4825195c18a
Anna <aniatychenko@gmail.com>
2020-12-08 03:16:37 +0200

    init homePage

---
+++ graphics-online-react/src/components/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import styles from './HomePage.module.scss'
+
+export default class HomePage extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {}
+    }
+    render()
+    {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
