0a274c8f695469122641f39f668c8af0e4f3f0ae
Daryna Matiiuk <dasha.matiyuk@gmail.com>
2020-12-11 22:25:29 +0200

    created graphic-employee page

---
+++ graphics-online-react/src/components/GraphicsEmployee/GraphicsEmployee.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from './GraphicsEmployee.module.scss'
+import Navbar from '../Navbar/Navbar'
+
+export default class GraphicsEmployee extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar></Navbar>
+                hello world!
+            </div>
+        )
+    }
+}
